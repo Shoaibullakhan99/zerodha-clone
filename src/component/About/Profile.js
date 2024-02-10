1422a1cb8dd440cf876bf02d136f44eb4cecb856
SHoaibullakhan99 <shoaibullakhan1999@outlook.com>
2024-02-10 14:42:50 +0530

    added toggle feature in bio

--- src/component/About/Profile.js
+++ src/component/About/Profile.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./about.module.css";
 
 export default function Profile({ data }) {
+  const [toggledIndexes, setToggledIndexes] = useState(
+    Array(data.data.length).fill(false)
+  );
+
+  function handleclick(index) {
+    setToggledIndexes((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  }
   return (
     <div className="row my-5">
-      {data.data.map((ele) => {
+      {data.data.map((ele, index) => {
         return (
           <div className="col-12 col-lg-4 text-center my-4">
             <img
@@ -13,6 +24,20 @@ export default function Profile({ data }) {
             ></img>
             <h5>{ele["name"]}</h5>
             <p>{ele["position"]}</p>
+            Bio
+            <button
+              style={{
+                border: "none",
+                marginLeft: "5px",
+                backgroundColor: "white",
+              }}
+              onClick={() => handleclick(index)}
+            >
+              &#8964;
+            </button>
+            {toggledIndexes[index] && (
+              <p className="mt-3">{ele["biography"]}</p>
+            )}{" "}
           </div>
         );
       })}
